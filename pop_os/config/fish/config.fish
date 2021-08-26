0b91b598dfa2e887fd4b5d9c1ce2a7a673463663
llytaii <llytaii@pop-ll-lap.fritz.box>
2021-08-26 16:07:24 +0200

    add starship config

---
+++ pop_os/config/fish/config.fish
@@ -0,0 +1,6 @@
+set fish_greeting
+
+starship init fish | source
+zoxide init fish | source
+alias ls='exa -l'
+alias update='sudo apt update && sudo apt upgrade && flatpak update && cargo install-update -a'
