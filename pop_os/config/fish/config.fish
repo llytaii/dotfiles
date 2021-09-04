95c41922902e0cb7be6c19894e60d8d76f932148
llytaii <llytaii@ll-pop-lap.fritz.box>
2021-09-04 12:20:56 +0200

    add rustup update to config.fish

--- pop_os/config/fish/config.fish
+++ pop_os/config/fish/config.fish
@@ -3,4 +3,4 @@ set fish_greeting
 starship init fish | source
 zoxide init fish | source
 alias ls='exa -l'
-alias update='sudo apt update && sudo apt upgrade && flatpak update && cargo install-update -a'
+alias update='sudo apt update && sudo apt upgrade && flatpak update && rustup update && cargo install-update -a'
