d76db5ab020c56c1e71e780dbfc5e113523dbab5
llytaii <llytaii@pm.me>
2021-10-28 20:45:19 +0200

    [add] gruvbox alacritty, yta alias

--- pop_os/config/fish/config.fish
+++ pop_os/config/fish/config.fish
@@ -2,5 +2,9 @@ set fish_greeting
 
 starship init fish | source
 zoxide init fish | source
+
 alias ls='exa -l'
-alias update='sudo apt update && sudo apt upgrade && flatpak update && rustup update && cargo install-update -a'
+alias lt='exa -l --tree --level 2'
+alias yta='youtube-dl -i --yes-playlist -x --audio-format mp3'
+
+alias update='sudo apt update && sudo apt upgrade && flatpak update && rustup update && cargo install-update -a && youtube-dl --update'
