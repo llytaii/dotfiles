b88f4b69da9cb28a28c41eaeda501b96edfa39db
llytaii <llytaii@pm.me>
2021-11-10 22:43:51 +0100

    [add] void linux dwm/slstatus/picom/feh etc

---
+++ void_linux/dwm/themes/tokyo-night.h
@@ -0,0 +1,11 @@
+static const char col_gray1[]       = "#1a1b26";
+static const char col_gray2[]       = "#1a1b26";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#f8f8f2";
+static const char col_gray5[]	    = "#aaaaaa";
+static const char col_cyan[]        = "#7aa2f7";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray5},
+};
