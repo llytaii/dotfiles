577eac13999f349ec34c47af5e628d45d442c6f5
llytaii <llytaii@pm.me>
2022-11-01 13:56:36 +0100

    [add] arch dotfiles

---
+++ arch/setup.sh
@@ -0,0 +1,4 @@
+sudo pacman -Syu telegram-desktop zoxide zellij fish tealdeer exa starship gcc cmake clang gdb nnn neofetch nevim helix qutebrowser keepassxc
+yay -Syu visual-studio-code-bin
+git config --global user.name "llytaii"
+git config --global user.email "llytaii@pm.me"
