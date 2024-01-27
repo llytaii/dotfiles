bec5a18d1604672aa0b5888478b300eeb175272c
llytaii <llytaii@pm.me>
2024-01-27 17:20:25 +0100

    update fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,15 @@
 set fish_greeting
-set -gx XKB_DEFAULT_LAYOUT 'de'
+# set -gx XKB_DEFAULT_LAYOUT 'de'
 
-starship init fish | source
+# starship init fish | source
 zoxide init fish | source
 
-alias cls='clear && ls'
+alias rpi3='ssh llytaii@192.168.188.46'
+# alias cls='clear && ls'
 alias ls='exa -l'
 alias lt='exa -l --tree --level 2'
-alias rman='rusty-man'
-alias hx='helix'
-alias yt_music='yt-dlp --download-archive ./archive.txt --ignore-errors --extract-audio --audio-format mp3  --output "%(title)s.%(ext)s" --yes-playlist (cat url)'
+# alias rman='rusty-man'
+# alias hx='helix'
+# alias yt_music='yt-dlp --download-archive ./archive.txt --ignore-errors --extract-audio --audio-format mp3  --output "%(title)s.%(ext)s" --yes-playlist (cat url)'
 
-thefuck --alias | source
+# thefuck --alias | source
