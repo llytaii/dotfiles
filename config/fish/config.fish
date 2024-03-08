e4b5aea11ee401fcd0d72dc3b8f68714eec64bb0
llytaii <llytaii@pm.me>
2024-03-08 09:09:34 +0100

    [mod] update sandbar_run to handle sandbar with pwr_stats + minor things

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,12 @@
 set fish_greeting
-# set -gx XKB_DEFAULT_LAYOUT 'de'
+set -gx XKB_DEFAULT_LAYOUT 'de'
 
-# starship init fish | source
+starship init fish | source
 zoxide init fish | source
 
-alias rpi3='ssh llytaii@192.168.188.46'
-# alias cls='clear && ls'
 alias ls='exa -l'
 alias lt='exa -l --tree --level 2'
-# alias rman='rusty-man'
-# alias hx='helix'
-# alias yt_music='yt-dlp --download-archive ./archive.txt --ignore-errors --extract-audio --audio-format mp3  --output "%(title)s.%(ext)s" --yes-playlist (cat url)'
+alias hx='helix'
 
-# thefuck --alias | source
+alias rpi3='ssh llytaii@192.168.188.46'
+alias yt_music='yt-dlp --download-archive ./archive.txt --ignore-errors --extract-audio --audio-format mp3  --output "%(title)s.%(ext)s" --yes-playlist (cat url)'
